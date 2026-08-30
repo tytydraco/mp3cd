e250a5638bd83e8a540f0436a5996802d4ff1346
Tyler Nijmeh <tylernij@gmail.com>
2026-08-30 09:22:20 -0500

    MPEG4 set GOP to 1s for bitrate stability
    Shorter GOP means slightly smaller I-frame sizes but more frequent high
    bitrate events, which is the failure mode for UID0018. UID0017 has a
    failure mode for >20Kb frame size, which is not significantly impacted
    by a 1s GOP.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/profiles/uid0018.dart
+++ lib/src/profiles/uid0018.dart
@@ -97,6 +97,7 @@ class Uid0018 extends Converter {
       ..pair('-sws_flags', 'accurate_rnd+full_chroma_int+full_chroma_inp')
       ..pair('-pix_fmt:v', 'yuv420p')
       ..pair('-b:v', '500k')
+      ..pair('-g:v', fps)
       ..pair('-r:v', fps)
       ..pair('-c:a', 'aac')
       ..pair('-ac:a', 1)
